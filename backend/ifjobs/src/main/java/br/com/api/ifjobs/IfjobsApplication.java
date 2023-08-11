36a1a8a03edb3514f466911c7c95abd39d529209
Evelyn Iglesias <evelyniglesias2004@gmail.com>
2023-08-11 12:04:13 -0300

    Criação do projeto de front (react) e back (java spring boot)

---
+++ backend/ifjobs/src/main/java/br/com/api/ifjobs/IfjobsApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.ifjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfjobsApplication.class, args);
+	}
+
+}
