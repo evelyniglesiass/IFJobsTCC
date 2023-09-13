344c1c93d66ae3473a10c1099a1c94c512b68d22
Hetielle Vitória de Matos <119357218+hetielle@users.noreply.github.com>
2023-09-13 19:26:25 -0300

    Alterando controller da empresa

--- backend/ifjobs/src/main/java/br/com/api/ifjobs/Controller/EmpresaController.java
+++ backend/ifjobs/src/main/java/br/com/api/ifjobs/Controller/EmpresaController.java
@@ -1,7 +1,10 @@
 package br.com.api.ifjobs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.ifjobs.models.Empresa;
@@ -11,12 +14,17 @@ import br.com.api.ifjobs.services.EmpresaService;
 public class EmpresaController {
     
     @Autowired
-    private EmpresaService es;
+    private EmpresaService empSer;
     
+    @PostMapping("/cadastrar/empresa")
+    public ResponseEntity<?> cadastrar(@RequestBody Empresa empresa){ 
+        return empSer.cadastrar(empresa); 
+    }
+
     //Listagem de empresas
     @GetMapping("/listarEmpresas")
     public Iterable<Empresa> listar(){
-        return es.listar();
+        return empSer.listar();
     }
 
     //Teste
