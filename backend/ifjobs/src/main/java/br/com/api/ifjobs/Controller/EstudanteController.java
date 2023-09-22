31403ea539b4ad635999c17b06b71baa37c4874e
Evelyn Iglesias <evelyniglesias2004@gmail.com>
2023-09-22 16:02:52 -0300

    Implementando rotas de listagem em Estudante

--- backend/ifjobs/src/main/java/br/com/api/ifjobs/Controller/EstudanteController.java
+++ backend/ifjobs/src/main/java/br/com/api/ifjobs/Controller/EstudanteController.java
@@ -1,8 +1,11 @@
 package br.com.api.ifjobs.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.ifjobs.models.Estudante;
 import br.com.api.ifjobs.models.Resposta;
+import br.com.api.ifjobs.repository.EstudanteRepository;
 import br.com.api.ifjobs.services.EstudanteService;
 import jakarta.validation.Valid;
 
@@ -19,6 +23,9 @@ public class EstudanteController {
 
     @Autowired
     private EstudanteService estSer;
+
+    @Autowired
+    private EstudanteRepository estRep;
 
     @PostMapping("/cadastrar/estudante")
     public ResponseEntity<?> cadastrar(@Valid @RequestBody Estudante estudante){ 
@@ -33,6 +40,17 @@ public class EstudanteController {
     @DeleteMapping("/remover/estudante/{id}") 
     public ResponseEntity<Resposta> remover(@PathVariable int id){ 
         return estSer.remover(id);
+    }
+
+    @GetMapping("/listar/pesquisa/{nome}")
+    public List<Estudante> listarPesquisa(@PathVariable String nome) {
+        return estRep.findByNomeContains(nome); 
+
+    }
+
+    @GetMapping("/listar/estudantes")
+    public List<Estudante> listarEstudantes() {
+        return estRep.listarEstudantes();
     }
   
 }
--- backend/ifjobs/src/main/java/br/com/api/ifjobs/Repository/EstudanteRepository.java
+++ backend/ifjobs/src/main/java/br/com/api/ifjobs/Repository/EstudanteRepository.java
@@ -1,5 +1,7 @@
 package br.com.api.ifjobs.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -13,9 +15,15 @@ public interface EstudanteRepository extends CrudRepository<Estudante, Integer>{
     int countByEmail(String email); // validar email   
 
     @Query(value = "SELECT Count(nome_usuario) FROM estudante WHERE nome_usuario = :usuario AND id <> :id", nativeQuery = true) 
-    int existeUsuario(String usuario, int id);
+    int existeUsuario(String usuario, int id); // validar nome de usuario
 
     @Query(value = "SELECT Count(email) FROM estudante WHERE email = :email AND id <> :id", nativeQuery = true) 
-    int existeEmail(String email, int id); 
+    int existeEmail(String email, int id); // validar email
+
+    @Query(value = "SELECT id, cidade, email,idade, nome, nome_usuario, senha, telefone FROM estudante", nativeQuery = true) 
+    List<Estudante> listarEstudantes(); // validar email
+
+    List<Estudante> findByNomeContains(String nome); // listar estudantes em uma pesquisa
+    List<Estudante> findAll(); // listar estudantes
 
 } 
